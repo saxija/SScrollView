aad04a9b6efa41fd9b8262feff13e9166878bf39
Sasa Jovanovic <sasa.jovanovic@delsystems.net>
2024-02-09 10:20:58 +0100

    Initial commit with cocoapod lib template

---
+++ Example/Pods/Target Support Files/Pods-SScrollView_Example/Pods-SScrollView_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SScrollView_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SScrollView_Example
+@end
---
+++ Example/Pods/Target Support Files/SScrollView/SScrollView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SScrollView : NSObject
+@end
+@implementation PodsDummy_SScrollView
+@end
